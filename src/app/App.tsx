53eedb8a85a1e5465e8b8eb4439f30e88f89ea11
bimurzaew <85867193+bimurzaew@users.noreply.github.com>
2023-05-13 15:18:16 +0300

    добавлена i18n интернационализация

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React, { Suspense }  from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import './styles/index.scss'
@@ -10,11 +10,13 @@ const App = () => {
     const {theme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <div className='content-page'>
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback=''>
+                <Navbar />
+                <div className='content-page'>
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
