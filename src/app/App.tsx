26af48106cdd8ff8e1bfbd17bfa323939c9a2e16
bimurzaew <85867193+bimurzaew@users.noreply.github.com>
2023-05-13 11:00:07 +0300

    navbar widget

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,16 @@
 import React  from 'react';
-import { Link} from 'react-router-dom'
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import './styles/index.scss'
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
             <button onClick={toggleTheme}>toggle</button>
-            <Link to='/'>Main</Link>
-            <Link to='/about'>About</Link>
             <AppRouter />
         </div>
     );
