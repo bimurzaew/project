fd99f7b1c75ce9b11354db518c82646a1b9edc76
bimurzaew <85867193+bimurzaew@users.noreply.github.com>
2023-05-18 15:23:44 +0300

    add bundle analyzer

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,7 +44,7 @@ module.exports = {
         '@typescript-eslint/naming-convention': 'off',
         '@typescript-eslint/no-misused-promises': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
-        'max-len': ['error', { ignoreComments: true }]
+        'max-len': ['error', { ignoreComments: true, code: 100 }]
     },
     globals: {
         __IS_DEV__: true
